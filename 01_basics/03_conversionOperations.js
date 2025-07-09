fcd8ca5cb54f505abb99037807481b1470cad045
namanalle7 <namanalle7@gmail.com>
2025-07-09 21:04:43 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -2,8 +2,8 @@ let score = "33"
 
 console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
 
 /*
 
@@ -15,8 +15,8 @@ true => 1/ false => 2
 
 let isLoggedIn = 1
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 /*
 
@@ -28,6 +28,41 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
 
+
+//************************************* OPERATIONS********************************
+
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+let str1 = "hello"
+let str2 = " naman"
+ let str3 = str1 + str2 
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2 , num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++ ;
+console.log(gameCounter);
