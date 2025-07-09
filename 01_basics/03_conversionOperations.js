4c7d0850f471bcb85441fc056e4a56acccd93f5b
namanalle7 <namanalle7@gmail.com>
2025-07-09 20:39:18 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,33 @@
+let score = "33"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+/*
+
+"33" => 33
+"33abc" => NaN
+true => 1/ false => 2
+
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+
+1 => true , 0 => false
+"" => false
+"naman" => true
+
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+
